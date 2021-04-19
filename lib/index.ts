ebc28708064f4c396f77b0911fa0eb1da266688e
keen.v <keen.w@outlook.com>
2021-04-19 23:01:19 +0800

    feat: add entry file

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import connect from './connect'
+import MessageChannel from './channel'
+import MChannelRouter from './router'
+import MChannelSession from './session'
+
+export {
+  connect,
+  MessageChannel,
+  MChannelRouter,
+  MChannelSession
+}
+
+export * from './channel'
+export * from './context'
